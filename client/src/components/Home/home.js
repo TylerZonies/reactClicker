d550b27d8a29365208c2863cee974d2ffe72ac11
Tyler Zonies <tylerzonies@gmail.com>
2018-07-12 10:23:43 -0600

    first commit

---
+++ client/src/components/Home/home.js
@@ -0,0 +1,26 @@
+import { React, Component} from 'react-dom';
+import { Row, Col, Navbar } from 'react-materialize';
+
+class Home extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            currentScore: 0,
+            topScore: 0,
+
+        }
+    }
+
+    componentDidMount(){
+
+    }
+
+    render(){
+        return(
+            <header>
+                
+            </header>
+                    
+        )
+    }
+}
---
+++ client/src/components/Result/result.js
@@ -0,0 +1,5 @@
+import { React, Component } from 'react-dom';
+
+Result = function(props){
+    
+}
